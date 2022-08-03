bf963a37f7d09b6473aa46a3f572a9d885e60e05
Maria Buznik <masha.vinogradova82@gmail.com>
2022-08-03 18:17:56 +0300

    add task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyColor = document.querySelector("body");
+const changeColorText = document.querySelector(".color");
+const changeColorBtn = document.querySelector(".change-color");
+
+changeColorBtn.addEventListener("click", handlerClick);
+function handlerClick() {
+  bodyColor.style.backgroundColor = getRandomHexColor();
+  changeColorText.textContent = getRandomHexColor();
+}
