f39230300303e0a6292108863e85286fe2453f88
Maria Buznik <masha.vinogradova82@gmail.com>
2022-08-03 18:00:51 +0300

    add task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,16 @@
+const formElem = document.querySelector(".login-form");
+formElem.addEventListener("submit", handlerSubmit);
 
+function handlerSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  if (email.value === "" || password.value === "") {
+    return alert("Please fill in all fields");
+  }
+
+  const result = { email: email.value, password: password.value };
+  console.log(result);
+  event.currentTarget.reset();
+}
